df82355f5b7f5cb3805f35ad3e83fccc82d1d36a
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-04-26 10:51:46 +0700

    1

--- database/migrations/2024_04_26_031814_create_carts_table.php
+++ database/migrations/2024_04_26_031814_create_carts_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignIdFor(Product::class)->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
