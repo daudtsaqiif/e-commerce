0d8451f6724e74c5aaa8574c7cfaad68497849b4
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-04-26 11:13:40 +0700

    1

--- database/migrations/2024_04_26_031834_create_transaction_items_table.php
+++ database/migrations/2024_04_26_031834_create_transaction_items_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,9 @@ return new class extends Migration
     {
         Schema::create('transaction_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Transaction::class)->references('id')->on('transactions')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignIdFor(Product::class)->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignIdFor(User::class)->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
