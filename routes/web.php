5c86075841fd43a850bc3963bbe02abfdd1a9e0f
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-05-06 14:20:08 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProducGalleryController;
 use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,9 @@ Route::name('admin.')->prefix('admin')->middleware('admin')->group(function(){
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
     Route::resource('/category', CategoryController::class)->except(['create', 'show', 'edit']);
     Route::resource('/product', ProductController::class);
+    Route::prefix('/product')->name('product.')->group(function(){
+        Route::resource('/gallery', ProducGalleryController::class);
+    });
 });
 
 Route::name('user.')->prefix('user')->middleware('user')->group(function(){
