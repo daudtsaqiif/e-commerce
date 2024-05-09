5c94a304c0bf807bea3ec140c09cef2258dad0fe
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-05-09 13:47:44 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Adim\MyTransactionController;
+use App\Http\Controllers\Adim\TransactionController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProducGalleryController;
 use App\Http\Controllers\Admin\ProductController;
@@ -13,16 +15,18 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::name('admin.')->prefix('admin')->middleware('admin')->group(function(){
+Route::name('admin.')->prefix('admin')->middleware('admin')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
     Route::get('/userList', [App\Http\Controllers\Admin\DashboardController::class, 'userList'])->name('userList');
     Route::put('/resetPassword/{id}', [App\Http\Controllers\Admin\DashboardController::class, 'resetPassword'])->name('resetPassword');
     Route::resource('/category', CategoryController::class)->except(['create', 'show', 'edit']);
     Route::resource('/product', ProductController::class);
-        Route::resource('/product.gallery', ProducGalleryController::class)->except(['create', 'show', 'edit', 'update']);
+    Route::resource('/product.gallery', ProducGalleryController::class)->except(['create', 'show', 'edit', 'update']);
+    Route::resource('/transaction', TransactionController::class);
+    Route::resource('/my-transaction', MyTransactionController::class)->only(['index', 'show']);
 });
 
-Route::name('user.')->prefix('user')->middleware('user')->group(function(){
+Route::name('user.')->prefix('user')->middleware('user')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\User\DashboardController::class, 'index'])->name('dashboard');
     Route::get('/updatePassword', [App\Http\Controllers\User\DashboardController::class, 'updatePassword'])->name('updatePassword');
     Route::put('/changePassword', [App\Http\Controllers\User\DashboardController::class, 'changePassword'])->name('changePassword');
