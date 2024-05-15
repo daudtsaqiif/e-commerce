e29b6f2c605163fedb6672e86517e9f2bac505a7
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-05-15 14:22:20 +0700

    1

--- app/Http/Controllers/FrontEnd/FrontEndController.php
+++ app/Http/Controllers/FrontEnd/FrontEndController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
@@ -11,6 +12,7 @@ class FrontEndController extends Controller
     //
     public function index(){
         $category = Category::select('id', 'name')->latest()->get();
+        $product = Product::with('product_galleries');
 
         return view('pages.frontend.index', compact('category'));
     }
