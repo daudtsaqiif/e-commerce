ac6be11a9d06a23f833bfca58bd6ee16de6ea0db
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-05-23 11:00:56 +0700

    1

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -13,6 +14,9 @@ class TransactionController extends Controller
     public function index()
     {
         //
+        $transaction = Transaction::select()->latest()->get();
+
+        return view('pages.admin.transaction.index' , compact('transaction'));
     }
 
     /**
