4ce673d2088f5023f3995a4d002dd9ac8e1253c7
daudtsaqiif <daudtsaqiif49@gmail.com>
2024-05-27 12:48:59 +0700

    1

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Support\Facades\Hash;
 
 class DashboardController extends Controller
 {
     //
     public function index(){
-        return view('pages.user.index');
+        $transaction = Transaction::count();
+        return view('pages.user.index', compact('transaction'));
     }
 
     public function updatePassword(){
--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -4,15 +4,67 @@
 
 @section('content')
 
-<div class="card">
-    <div class="card-body">
-        <div class="text-end">
-            <p class="badge bg-info text-dark mt-2"><i class="bi bi-info-circle me-1"></i> | {{ Auth::user()->role }}</p>
+    <div class="card">
+        <div class="card-body">
+            <div class="text-end">
+                <p class="badge bg-info text-dark mt-2"><i class="bi bi-info-circle me-1"></i> | {{ Auth::user()->role }}</p>
+            </div>
+            <h5 class="text-center fs-4 fw-bold">Hallo {{ Auth::user()->name }}</h5>
+            <h4 class="text-center fs-5 fw-semibold"> {{ Auth::user()->email }}</h4>
+            <p class="text-center fs-6 fw-medium">{{ Auth::user()->created_at }}</p>
         </div>
-        <h5 class="text-center fs-4 fw-bold">Hallo {{ Auth::user()->name }}</h5> 
-        <h4 class="text-center fs-5 fw-semibold"> {{ Auth::user()->email }}</h4>
-        <p class="text-center fs-6 fw-medium">{{ Auth::user()->created_at }}</p>
     </div>
-</div>
+
+    <div class="section dashboard">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Pending</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                <i class="bi bi-cart"></i>
+                            </div>
+                            <div class="ps-3">
+                                <h6>{{ $transaction }}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Settlement</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                <i class="bi bi-cart"></i>
+                            </div>
+                            <div class="ps-3">
+                                <h6>{{ $transaction }}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Expired</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                <i class="bi bi-cart"></i>
+                            </div>
+                            <div class="ps-3">
+                                <h6>{{ $transaction }}</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </div>
 
 @endsection
